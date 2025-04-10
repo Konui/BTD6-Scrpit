a3043790198d6a63b53faa225d1854440f9c86a7
Kion <szybchen@gmail.com>
2025-04-10 22:30:26 +0800

    feat: 修改upgrade解析逻辑

--- actions/Upgrade.py
+++ actions/Upgrade.py
@@ -1,4 +1,5 @@
 from actions import Action
+import copy
 
 class Upgrade(Action):
     def __init__(self, game, line):
@@ -28,12 +29,16 @@ class Upgrade(Action):
         self.game.keyboard_tap([',','.','/'][self.path])
 
     def parse(self, parts, script):
-        self.path = int(parts[1]) - 1
-        positions = parts[2].split(",")
+        positions = parts[1].split(",")
         if len(positions) == 2:
             self.x = int(positions[0])
             self.y = int(positions[1])
         else:
-            x,y = script.positions[parts[2]]
+            x,y = script.positions[parts[1]]
             self.x = x
             self.y = y
+
+        self.path = int(parts[2]) - 1
+        if len(parts) > 3:
+            for _ in range(1, int(parts[3])):
+                script.actions.append(copy.copy(self))
